205ca74ebdd5c92bb915ef2d69e94bb289077305
Peter White <petras9789@gmail.com>
2024-05-10 15:11:12 -0600

    fix: small fixes after rebase

--- node/src/rpc/mod.rs
+++ node/src/rpc/mod.rs
@@ -7,7 +7,7 @@
 
 use std::sync::Arc;
 
-use parachain_template_runtime::{opaque::Block, AccountId, Balance, Nonce};
+use frontier_parachain_runtime::{opaque::Block, AccountId, Balance, Nonce};
 
 use sc_client_api::{
     backend::{AuxStore, Backend, StorageProvider},
